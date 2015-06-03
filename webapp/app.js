693cb09b44ece1a9206ae70585206ebd3b4405bd
LiJohnson <598420668@163.com>
2015-06-03 17:09:02 +0800

    添加websocket

--- webapp/app.js
+++ webapp/app.js
@@ -3,38 +3,42 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express'),
+	routes = require('./routes');
 
 var app = module.exports = express.createServer();
-
+io = require('socket.io')(app)
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 // Routes
 
 app.get('/', routes.index);
 app.get("/hi",function(req,res){
-  res.write("hi lcs");
-  res.end();
+	res.write("hi lcs");
+	res.end();
 })
 
+io.on("connection",function(s){
+	console.log(s);
+});
+
 app.listen(9595, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
