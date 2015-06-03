d515c0fbf06192940353eb8e261c50f891ee65cd
LiJohnson <598420668@163.com>
2015-06-03 19:50:52 +0800

    优化页面显示

--- webapp/app.js
+++ webapp/app.js
@@ -33,11 +33,25 @@ app.get('/', routes.index);
 app.get("/hi",function(req,res){
 	res.write("hi lcs");
 	res.end();
-})
-
-io.on("connection",function(s){
-	console.log(s);
 });
+;
+
+io.on("connection",(function(){
+	var socketMap = {};
+	app.post("/upload",function(req,res){
+		for( var id in socketMap ){
+			socketMap[id].emit("data",req.body.data)
+		}
+		res.write("uploaded");
+		res.end();
+	});
+	return function(socket){
+		socketMap[socket.id] = socket;
+		socket.on("disconnect",function(){
+			delete socketMap[socket.id];
+		});
+	}
+})());
 
 app.listen(9595, function(){
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
